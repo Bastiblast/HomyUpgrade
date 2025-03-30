50ae437322854d6beffb9f07938bae7c901f180f
bastuce <bastuce.commit@gmail.com>
2025-03-30 08:26:45 +0200

    Routing installed.

--- src/main.tsx
+++ src/main.tsx
@@ -2,10 +2,60 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 import React from 'react'
+import { Link, Outlet, RouterProvider, createRootRoute, createRoute, createRouter } from '@tanstack/react-router'
 
-// document.body.style.display = "none"
+// Import the generated route tree
+
+const rootRoute = createRootRoute({
+  component: () => (
+    <>
+      <div className="flex gap-2 p-2">
+        <Link to="/monkeyWhiteBoard" className="[&.active]:font-bold">
+          Home
+        </Link>{' '}
+        <Link to="/monkeyWhiteBoard/about" className="[&.active]:font-bold">
+          About
+        </Link>
+      </div>
+      <hr />
+      <Outlet />
+    </>
+  ),
+})
+
+const indexRoute = createRoute({
+  getParentRoute: () => rootRoute,
+  path: '/monkeyWhiteBoard/',
+  component: function Index() {
+    return (
+      <div className="p-2">
+        <h3>Welcome Home!</h3>
+      </div>
+    )
+  },
+})
+
+const aboutRoute = createRoute({
+  getParentRoute: () => rootRoute,
+  path: '/monkeyWhiteBoard/about',
+  component: function About() {
+    return <div className="p-2">Hello from About!</div>
+  },
+})
+
+const routeTree = rootRoute.addChildren([indexRoute, aboutRoute])
+
+const router = createRouter({ routeTree })
+
+declare module '@tanstack/react-router' {
+  interface Register {
+    router: typeof router
+  }
+}
 
 const newBody = document.createElement("section")
+
+document.body.style.display = "none"
 
 document.querySelector("html")?.prepend(newBody)
 
@@ -17,6 +67,6 @@ createRoot(
   })(),
 ).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 );
