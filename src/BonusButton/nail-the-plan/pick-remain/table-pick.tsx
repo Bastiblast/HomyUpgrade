7121c07b6d85eb9397ca8b26bc7536afb16c5865
bastuce <bastuce.commit@gmail.com>
2025-08-02 09:53:14 +0200

    tailwind and shadcn working, need to refactor.

--- src/BonusButton/nail-the-plan/pick-remain/table-pick.tsx
+++ src/BonusButton/nail-the-plan/pick-remain/table-pick.tsx
@@ -1,11 +1,25 @@
 import React, { useContext } from 'react'
 import {DataCenterContext} from '../../../query/datacenter-contextAndProvider'
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from '@/components/ui/card'
 
 export default function TablePick() {
 
     const {pickSummary} = useContext(DataCenterContext)
 
   return (
-    <div onClick={() => pickSummary.getpickResponse()}>{pickSummary && JSON.stringify(pickSummary)}</div>
+    <Card>
+      <CardHeader>
+        <CardTitle>
+          Pick Summary
+        </CardTitle>
+        <CardDescription>
+          see pick remain
+        </CardDescription>
+      </CardHeader>
+      <CardContent>
+      <div onClick={() => pickSummary.getpickResponse()}>{pickSummary && JSON.stringify(pickSummary)}</div>
+
+      </CardContent>
+    </Card>
   )
 }
